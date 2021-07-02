11da3fa168aacd5fb23f810db441b65f44e4d41d
Alberto <ajsaenz1091@gmail.com>
2021-07-01 20:58:48 -0400

    commit tree 6b6265548c7b91aff16e4cb9a0dd0239669c8546

---
+++ lib/author.rb
@@ -0,0 +1,22 @@
+class Author
+    #Higher level abstraction
+
+    attr_accessor :name
+
+    #Mid level abstraction
+    
+    # attr_writer :name
+    # attr_reader :name
+
+    #Lower level abstraction
+
+    #setter method
+    # def name=(name)
+    #     @name= name
+    # end
+
+    # def name
+    #     @name
+    # end
+
+end
